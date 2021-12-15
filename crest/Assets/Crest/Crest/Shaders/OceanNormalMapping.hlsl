7b310e8752c512a2fa8a49db6f65d45ae83e4edf
Dale Eidd <dale@eidd.name>
2021-12-15 02:00:42 -0800

    Mod offset for normals

--- crest/Assets/Crest/Crest/Shaders/OceanNormalMapping.hlsl
+++ crest/Assets/Crest/Crest/Shaders/OceanNormalMapping.hlsl
@@ -14,7 +14,7 @@ half2 SampleNormalMaps(float2 worldXZUndisplaced, float lodAlpha, in const Casca
 
 	const float2 v0 = float2(0.94, 0.34), v1 = float2(-0.85, -0.53);
 
-	worldXZUndisplaced -= _CrestFloatingOriginOffset.xz;
+	worldXZUndisplaced -= _CrestFloatingOriginOffset.xz % (_NormalsScale * 2.0 * lodDataGridSize);
 
 	float nstretch = _NormalsScale * lodDataGridSize; // normals scaled with geometry
 	const float spdmulL = normalScrollSpeeds[0];
